f01de6af16a84da51525741c27480be2079b1998
JPS9380 <jatinp.singh2003@gmail.com>
2025-07-04 16:14:26 +0530

    implementing ANN and added more synthetic data

--- script.py
+++ script.py
@@ -8,7 +8,6 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
@@ -17,6 +16,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 tfv = TfidfVectorizer()
 ps = PorterStemmer()
 stop_words = set(stopwords.words('english'))
+stop_words.remove("not")
 
 #defining a function to randomly take out a sample from HateSpeechDataset.csv
 def sampling(df, n):
